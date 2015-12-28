0c86adfda65013f1e9eeece39ad93c9fbf4b2b32
Alexander Wylie <alex@thewidgetworks.co.uk>
2015-12-28 16:31:35 +0000

    Parsing of Meteor.Settings and environment variables for Tesseract binary path and language prefixes

--- meteor-tesseract.js
+++ meteor-tesseract.js
@@ -1,29 +1,57 @@
+meteorTesseract = {};
+
 Meteor.startup(function() {
 	"use strict";
 	if (Meteor.isServer) {
 		(() => {
+			function getPathKey ( ) {
+				return Object.keys(this || {}).map( key => {
+					if ( /^tesseract_?path/i.test( key ) ) {
+					 return this[key];
+					}
+				}, this);
+			}
+
+			function getPrefixKey ( ) {
+				return Object.keys(this || {}).map( key => {
+					if (/^tessdata_?prefix/i.test( key )) {
+					 return this[key];
+					}
+				}, this);
+			}
 
 			try {
 				const DEBUG = process.env.NODE_ENV !== 'production';
-
-				const fs = require('fs');
-				const prefixes = Object.keys(process.env).reduce((prev, curr)=> {
-					if (/^TESSDATA_/.test(curr)) {
-						prev.push(curr);
-					}
-					return prev;
-				}, ['eng']);
-
-				let tesseract = require('node-tesseract');
+				const fs = Npm.require('fs');
+				let tesseract = Npm.require('node-tesseract');
 				let stats;
-				let tesseractPath = (
-						Meteor.settings.private.hasOwnProperty('tesseract') &&
-							Meteor.settings.private.tesseract ) ||
-								( Meteor.settings.hasOwnProperty('tesseract') &&
-									Meteor.settings.tesseract ) ||
-										process.env.tesseract;
+				// Try to get Tesseract Location from Meteor settings or environment variables
+				let hostTesseract = (function() {
+					let binary = [],
+						prefixes = [];
+					if ( Meteor.settings ) {
+						if (Meteor.settings.private) {
+							binary = getPathKey.call( Meteor.settings.private );
+							prefixes = getPrefixKey.call( Meteor.settings.private );
+						} else if (Meteor.settings.public) {
+							binary = getPathKey.call( Meteor.settings.public );
+							prefixes = getPrefixKey.call( Meteor.settings.public );
+						} else {
+							binary = getPathKey.call( Meteor.settings );
+							prefixes = getPrefixKey.call( Meteor.settings );
+						}
+					} else if ( process.env ) {
+						binary = getPathKey.call( process.env );
+						prefixes = getPrefixKey.call( process.env );
+					}
+					//console.log(binary);
+					return {
+						binaryPath : ( binary.length && binary[0] ) || false,
+						prefixes : ( prefixes.length || false )
+					};
+				})();
 
-				if ( tesseractPath ) {
+				if ( hostTesseract.binaryPath ) {
 					try {
 						stats = fs.lstatSync(tesseractPath);
 						// FS stats to be deprecated
@@ -38,7 +66,7 @@ Meteor.startup(function() {
 				}
 
 				if ( typeof tesseract !== 'undefined') {
-					tesseract = Object.create(tesseract, {
+					meteorTesseract = Object.create(tesseract, {
 						translateText : {
 							value (image, l = 'eng', psm = 6, binary = tesseractPath ) {
 
@@ -47,9 +75,13 @@ Meteor.startup(function() {
 									return false;
 								}
 
-								if ( prefixes.length && ~prefixes.indexOf(l) ) {
+								if ( hostTesseract.prefixes.length && ~hostTesseract.prefixes.indexOf(l) ) {
 									DEBUG && console.log("Tesseract Language prefix not installed. Defaulting to English.");
 									l = 'eng';
+								} else if ( DEBUG ) {
+									Object.keys(hostTesseract.prefixes).foreach( prefix => {
+										console.log("Language Prefix found :", prefix);
+									});
 								}
 
 								const options = {
@@ -74,14 +106,14 @@ Meteor.startup(function() {
 							enumerable: false
 						}
 					});
-					return tesseract;
+					return meteorTesseract;
 				} else {
 					throw new Meteor.Error("Tesseract is undefined");
 				}
 
-			} catch ( e ) {
+			} catch ( err ) {
 				if ( err instanceof Meteor.Error ) {
-					console.log(err.reason, err.details);
+					console.log("Tesseract MeteorError : ", err.message || err.details || err.reason || err);
 				} else {
 					console.log("Tesseract Error : ", err);
 				}
--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'awylie199:meteor-tesseract',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Server side optical character recognition (OCR) using Tesseract',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/awylie199/meteor-tesseract.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
@@ -17,10 +13,8 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-
-  api.addFiles(['meteor-tesseract.js'],'server');
-
-  api.export('tesseract');
+  api.addFiles(['meteor-tesseract.js'], 'server');
+  api.export('meteorTesseract');
 });
 
 Package.onTest(function(api) {
